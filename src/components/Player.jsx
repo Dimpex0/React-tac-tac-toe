bf2ded68cf31e0e30c055ce47487dabb0127da22
Dimpex <mitkodd2005@gmail.com>
2023-11-14 12:16:08 +0200

    initial commit

---
+++ src/components/Player.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+export default function Player({
+  initialName,
+  symbol,
+  isActive,
+  onChangeName,
+}) {
+  const [name, setName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  function clickHandler() {
+    setIsEditing((editing) => !editing);
+    if (isEditing) {
+      onChangeName(symbol, name);
+    }
+  }
+
+  function handleChange(event) {
+    setName(event.target.value);
+  }
+
+  let playerName = <span className="player-name">{name}</span>;
+
+  if (isEditing) {
+    playerName = (
+      <input type="text" required value={name} onChange={handleChange} />
+    );
+  }
+
+  return (
+    <li className={isActive ? "active" : undefined}>
+      <span className="player">
+        {playerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={clickHandler}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
+}
